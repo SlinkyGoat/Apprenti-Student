ea2b291e30e1e2bf87d3259e761b3076079b9917
SlinkyGoat <adamustby@gmail.com>
2025-08-20 16:44:26 -0500

    AU: completed text adventure lab, and shopping cart arrays/methods

--- java-fundamentals/exercise-shopping-cart-arrays-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
+++ java-fundamentals/exercise-shopping-cart-arrays-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -5,18 +5,74 @@ import java.util.Scanner;
 public class ShoppingCartApp {
   public static void main(String[] args) {
     Scanner console = new java.util.Scanner(System.in);
+    int shippingAddress;
+    int orderQuantity;
+    int size;
 
     System.out.println("Welcome to the shopping cart app!");
 
     // Create arrays to contain addresses and sizes
+      String[] addresses = {"1600 Pennsylvania Avenue, Washington DC", "11 Wall Street New York, NY", "350 Fifth Avenue New York, NY 10118",
+              "221 B Baker St, London, England", "Tour Eiffel Champ de Mars, Paris"};
+      String[] sizes = {"Small", "Medium", "Large", "Extra Large", "Humongous"};
 
     // Prompt for shipping address
+      for (int i = 0; i < addresses.length; i++) {
+          System.out.println((i + 1) + ". " + addresses[i]);
+      }
+      System.out.println("Shipping address?");
+      while(true){
+          try{
+              shippingAddress = Integer.parseInt(console.nextLine());
+              if(shippingAddress < 1 || shippingAddress > addresses.length){
+                  System.out.println("Please enter a valid number 1-" + addresses.length);
+              } else{
+                  break;
+              }
+          } catch (Exception e){
+              System.out.println("Please enter a number.");
+          }
+      }
+
+      // Prompt for quantity
+      System.out.println("Quantity?");
+      while(true){
+          try{
+              orderQuantity = Integer.parseInt(console.nextLine());
+              if(orderQuantity < 0){
+                  System.out.println("Please enter a valid non-negative number");
+              } else{
+                  break;
+              }
+          } catch (Exception e){
+              System.out.println("Please enter a number.");
+          }
+      }
 
     // Prompt for Size
+      for (int i = 0; i < sizes.length; i++) {
+          System.out.println((i + 1) + ". " + sizes[i]);
+      }
+      System.out.println("Size?");
+      while(true){
+          try{
+              size = Integer.parseInt(console.nextLine());
+              if(size < 1 || size > sizes.length){
+                  System.out.println("Please enter a valid number 1-" + sizes.length);
+              } else{
+                  break;
+              }
+          } catch (Exception e){
+              System.out.println("Please enter a number.");
+          }
+      }
 
     // Print details
     System.out.println("\nDetails:");
+      System.out.println("Shipping to: \t" + addresses[shippingAddress - 1]); // user entered number starting from 1 but array starts from 0
+      System.out.println("Order quantity: " + orderQuantity);
+      System.out.println("Size: \t\t\t" + sizes[size - 1]); // user entered number starting from 1 but array starts from 0
 
-    System.out.println("Bye");
+    System.out.println("\nBye");
   }
 }
--- java-fundamentals/exercise-shopping-cart-methods-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
+++ java-fundamentals/exercise-shopping-cart-methods-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -1,5 +1,7 @@
 package org.example.shoppingcart;
 
+import java.util.Scanner;
+
 /**
  * Refactor the code to use the following methods:
  * 1. Display a list of choices from an array. Returns nothing. Needs an array
@@ -8,7 +10,7 @@ package org.example.shoppingcart;
  * the user with.
  * 3. Prompt the user for an integer. Returns an integer. Needs a string to
  * prompt the user with.
- *
+ * <p>
  * Note: We will not create method(s) for calculating the total cost until we
  * learn more about Object-Oriented Programming and some additional data
  * structures.
@@ -16,63 +18,70 @@ package org.example.shoppingcart;
 
 public class ShoppingCartApp {
 
-  public static void main(String[] args) {
-    System.out.println("Welcome to the shopping cart app!");
-    String[] addresses = { "123 Main St", "456 Main St", "789 Main St" };
-    String[] sizes = { "small", "medium", "large" };
+    public static void main(String[] args) {
+        System.out.println("Welcome to the shopping cart app!");
+        String[] addresses = {"123 Main St", "456 Main St", "789 Main St"};
+        String[] sizes = {"small", "medium", "large"};
 
-    java.util.Scanner console = new java.util.Scanner(System.in);
-    boolean confirm = false;
-    String taxExempt = "";
-    String shipping = "";
-    String promoCode = "";
+        java.util.Scanner console = new java.util.Scanner(System.in);
+        boolean confirm = false;
+        String taxExempt = "";
+        String shipping = "";
+        String promoCode = "";
 
-    while (!confirm) {
-      int addressIndex = 0;
-      int sizeIndex = 0;
-      // Prompt for tax exempt
-      System.out.println("Are you tax-exempt? (y/n)");
-      taxExempt = console.nextLine();
+        while (!confirm) {
+            int addressIndex = 0;
+            int sizeIndex = 0;
+            // Prompt for tax exempt
+            taxExempt = promptUserForString("Are you tax-exempt? (y/n)");
 
-      // Prompt for shipping address
-      for (int i = 0; i < addresses.length; i++) {
-        System.out.println((i + 1) + ". " + addresses[i]);
-      }
-      System.out.println("Shipping address?");
-      String address = console.nextLine();
-      addressIndex = Integer.parseInt(address);
-      // Prompt for shipping
-      System.out.println("Shipping? (standard/overnight/twoday)");
-      shipping = console.nextLine();
+            // Prompt for shipping address
+            displayChoices(addresses);
+            addressIndex = promptUserForInt("Shipping address?");
 
-      // Prompt for order quantity
-      System.out.println("Order quantity?");
-      int orderQuantity = Integer.parseInt(console.nextLine());
+            // Prompt for shipping
+            shipping = promptUserForString("Shipping? (standard/overnight/twoday)");
 
-      // Prompt for Size
-      for (int i = 0; i < sizes.length; i++) {
-        System.out.println((i + 1) + ". " + sizes[i]);
-      }
-      System.out.println("Size?");
-      String size = console.nextLine();
-      sizeIndex = Integer.parseInt(size);
+            // Prompt for order quantity
+            int orderQuantity = promptUserForInt("Order quantity?");
 
-      // Prompt for promo code
-      System.out.println("Promo code for free shipping?");
-      promoCode = console.nextLine();
+            // Prompt for Size
+            displayChoices(sizes);
+            sizeIndex = promptUserForInt("Size?");
 
-      // Print details
-      System.out.println("\nDetails:");
-      System.out.println("Tax-exempt: " + taxExempt);
-      System.out.println("Address: " + addresses[addressIndex - 1]);
-      System.out.println("Shipping: " + shipping);
-      System.out.println("Size: " + sizes[sizeIndex - 1]);
-      System.out.println("Order quantity: " + orderQuantity);
-      System.out.println("Promo code: " + promoCode);
-      System.out.println("Confirm Order y/n");
-      confirm = "y".equals(console.nextLine());
+            // Prompt for promo code
+            promoCode = promptUserForString("Promo code for free shipping?");
+
+            // Print details
+            System.out.println("\nDetails:");
+            System.out.println("Tax-exempt: " + taxExempt);
+            System.out.println("Address: " + addresses[addressIndex - 1]);
+            System.out.println("Shipping: " + shipping);
+            System.out.println("Size: " + sizes[sizeIndex - 1]);
+            System.out.println("Order quantity: " + orderQuantity);
+            System.out.println("Promo code: " + promoCode);
+            System.out.println("Confirm Order y/n");
+            confirm = "y".equals(console.nextLine());
+        }
+
+        System.out.println("Bye");
     }
 
-    System.out.println("Bye");
-  }
+    private static void displayChoices(String[] choices) {
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println((i + 1) + ". " + choices[i]);
+        }
+    }
+
+    private static String promptUserForString(String prompt) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(prompt);
+        return in.nextLine();
+    }
+
+    private static int promptUserForInt(String prompt) {
+        String response = promptUserForString(prompt);
+        return Integer.parseInt(response);
+    }
+
 }
