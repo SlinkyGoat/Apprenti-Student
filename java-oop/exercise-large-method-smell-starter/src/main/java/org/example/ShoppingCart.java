ef28c09a205639fe01c1930921cb73d9bffeb236
SlinkyGoat <adamustby@gmail.com>
2025-09-02 17:48:08 -0500

    method smells complete

--- java-oop/exercise-large-method-smell-starter/src/main/java/org/example/ShoppingCart.java
+++ java-oop/exercise-large-method-smell-starter/src/main/java/org/example/ShoppingCart.java
@@ -2,18 +2,24 @@ package org.example;
 
 public class ShoppingCart {
     public double checkoutShoppingCart (Item[] items, double taxRate, double discountCode) {
+        return applyTax(applyDiscount(subTotal(items), discountCode), taxRate);
+    }
 
-        double subtotal = 0.00;
-        for (int i = 0; i < items.length; i++) {
-            subtotal += items[i].getPrice();
+    public double applyTax(double currentTotal, double taxRate){
+        double tax = currentTotal * taxRate;
+        return currentTotal + tax;
+    }
+
+    public double applyDiscount(double currentTotal, double discount){
+        discount *= currentTotal;
+        return currentTotal - discount;
+    }
+
+    public double subTotal(Item[] items){
+        double total = 0.0;
+        for(Item item : items){
+            total += item.getPrice();
         }
-
-        double discount = subtotal * discountCode;
-
-        subtotal -= discount;
-
-        double tax = (subtotal) * taxRate;
-
-        return subtotal + tax;
+        return total;
     }
 }
