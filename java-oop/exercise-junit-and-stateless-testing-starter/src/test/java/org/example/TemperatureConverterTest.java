c6f0c724900f23be1b2ba4f87aa98550851bb73b
SlinkyGoat <adamustby@gmail.com>
2025-09-09 14:09:35 -0500

    stateless exercise

--- java-oop/exercise-junit-and-stateless-testing-starter/src/test/java/org/example/TemperatureConverterTest.java
+++ java-oop/exercise-junit-and-stateless-testing-starter/src/test/java/org/example/TemperatureConverterTest.java
@@ -1,9 +1,58 @@
 package org.example;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TemperatureConverterTest {
     private TemperatureConverter converter;
+
+    private final double FAHRENHEIT_TEMPERATURE = 77.0;
+    private final double CELSIUS_TEMPERATURE = 25.0;
+    private final double KELVIN_TEMPERATURE = 298.15;
+
+    @BeforeEach
+    public void setup(){
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    public void FahrenheitToCelsius(){
+        double actualResult = converter.fahrenheitToCelsius(FAHRENHEIT_TEMPERATURE);
+        assertEquals(CELSIUS_TEMPERATURE, actualResult);
+    }
+
+    @Test
+    public void FahrenheitToKelvin(){
+        double actualResult = converter.fahrenheitToKelvin(FAHRENHEIT_TEMPERATURE);
+        assertEquals(KELVIN_TEMPERATURE, actualResult);
+    }
+
+    @Test
+    public void CelsiusToFahrenheit(){
+        double actualResult = converter.celsiusToFahrenheit(CELSIUS_TEMPERATURE);
+        assertEquals(FAHRENHEIT_TEMPERATURE, actualResult);
+    }
+
+    @Test
+    public void CelsiusToKelvin(){
+        double actualResult = converter.celsiusToKelvin(CELSIUS_TEMPERATURE);
+        assertEquals(KELVIN_TEMPERATURE, actualResult);
+    }
+
+    @Test
+    public void KelvinToCelsius(){
+        double actualResult = converter.kelvinToCelsius(KELVIN_TEMPERATURE);
+        assertEquals(CELSIUS_TEMPERATURE, actualResult);
+    }
+
+    @Test
+    public void KelvinToFahrenheit(){
+        double actualResult = converter.kelvinToFahrenheit(KELVIN_TEMPERATURE);
+        assertEquals(FAHRENHEIT_TEMPERATURE, actualResult);
+    }
 
 
 }
