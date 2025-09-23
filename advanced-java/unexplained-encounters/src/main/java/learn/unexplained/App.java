ae3881f4a4d0bab6fb5e78d111175e6009cfaea6
SlinkyGoat <adamustby@gmail.com>
2025-09-23 10:03:30 -0500

    code along and spring xml config exercise

--- advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
+++ advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
@@ -4,17 +4,23 @@ import learn.unexplained.data.EncounterFileRepository;
 import learn.unexplained.domain.EncounterService;
 import learn.unexplained.ui.Controller;
 import learn.unexplained.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        EncounterFileRepository repository = new EncounterFileRepository("./data/encounters.csv");
+//        EncounterFileRepository repository = new EncounterFileRepository("./data/encounters.csv");
+//
+//        // Instantiate and Run Controller
+//        EncounterService service = new EncounterService(repository);
+//
+//        View view = new View();
+//        Controller controller = new Controller(service, view);
 
-        // Instantiate and Run Controller
-        EncounterService service = new EncounterService(repository);
-
-        View view = new View();
-        Controller controller = new Controller(service, view);
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("dependency-configuration.xml");
+        Controller controller = context.getBean(Controller.class);
 
         controller.run();
     }
