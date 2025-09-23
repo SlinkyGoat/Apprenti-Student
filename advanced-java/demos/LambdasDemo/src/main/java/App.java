1332b84906aa247761907efd517e779bb9b50017
SlinkyGoat <adamustby@gmail.com>
2025-09-23 15:07:07 -0500

    lambdas

--- advanced-java/demos/LambdasDemo/src/main/java/App.java
+++ advanced-java/demos/LambdasDemo/src/main/java/App.java
@@ -2,9 +2,11 @@
 
 public class App {
     public static void main(String[] args) {
-       LegacyFilter legacyFilter = new LegacyFilter();
-       legacyFilter.filterAddresses();
+//       LegacyFilter legacyFilter = new LegacyFilter();
+//       legacyFilter.filterAddresses();
 
+       LambdaFilter lambdaFilter = new LambdaFilter();
+       lambdaFilter.filterLambdas();
     }
 
 }
